8573d00af80f457bb1dd21ad45f492af0c4bfb45
Saleh7127 <abusaleh@appscode.com>
2023-05-09 15:27:33 +0600

    C10:Channel

--- golang-book/random_prac.go
+++ golang-book/random_prac.go
@@ -1,5 +1,7 @@
-package main
+/*package main
 
+import "fmt"
+*/
 /*func main() {
 	var out []*int
 	for i := 0; i < 3; i++ {
@@ -54,9 +56,9 @@ func DeduplicateStrings(slice []string) []string {
 	fmt.Println(result)
 
 }
-*/
-// moveToFront moves needle to the front of haystack, in place if possible.
+
 func moveToFront(needle string, haystack []string) []string {
+
 	if len(haystack) != 0 && haystack[0] == needle {
 		return haystack
 	}
@@ -79,9 +81,130 @@ func moveToFront(needle string, haystack []string) []string {
 
 func main() {
 
-	/*haystack := []string{"a", "b", "c", "d", "e"} // [a b c d e]
+	haystack := []string{"a", "b", "c", "d", "e"} // [a b c d e]
 	haystack = moveToFront("c", haystack)         // [c a b d e]
 	haystack = moveToFront("f", haystack)         // [f c a b d e]
-	fmt.Println(haystack)*/
+	fmt.Println(haystack)
 
+}
+
+*/
+
+/*package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	data1 := "ABC"
+	fmt.Println(utf8.ValidString(data1)) //prints: true
+
+	data2 := "A\xfeC"
+	fmt.Println(utf8.ValidString(data2)) //prints: false
+}
+*/
+
+/*
+package main
+
+import (
+	"fmt"
+)
+
+type Employee struct {
+	name string
+	age  int
+}
+
+func (e Employee) changeName(newName string) {
+	e.name = newName
+}
+func (e *Employee) changeAge(newAge int) {
+	e.age = newAge
+}
+func main() {
+	e := Employee{
+		name: "Mark Andrew",
+		age:  50,
+	}
+	fmt.Printf("Employee name before change: %s", e.name)
+	e.changeName("Michael Andrew")
+	fmt.Printf("\nEmployee name after change: %s", e.name)
+
+	fmt.Printf("\n\nEmployee age before change: %d", e.age)
+	e.changeAge(51)
+	fmt.Printf("\nEmployee age after change: %d", e.age)
+}
+
+*/
+
+/*package main
+
+import (
+	"fmt"
+)
+
+func hello() {
+	fmt.Println("Hello world goroutine")
+}
+func main() {
+	go hello()
+	fmt.Println("main function")
+	var cc int
+	fmt.Scanf("%d", &cc)
+}
+*/
+
+/*
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func numbers() {
+	for i := 1; i <= 5; i++ {
+		time.Sleep(time.Second)
+		fmt.Printf("%d ", i)
+	}
+}
+func alphabets() {
+	for i := 'a'; i <= 'e'; i++ {
+		time.Sleep(time.Second)
+		fmt.Printf("%c ", i)
+	}
+}
+func main() {
+	var a chan int
+	if a == nil {
+		fmt.Println("channel a is nil, going to define it")
+		a = make(chan int)
+		fmt.Printf("Type of a is %T", a)
+	}
+}
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func hello(done chan bool) {
+	fmt.Println("hello go routine is going to sleep")
+	time.Sleep(4 * time.Second)
+	fmt.Println("hello go routine awake and going to write to done")
+	done <- true
+}
+func main() {
+	done := make(chan bool)
+	fmt.Println("Main going to call hello go goroutine")
+	go hello(done)
+	<-done
+	fmt.Println("Main received data")
 }
